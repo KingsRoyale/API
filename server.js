9c009d02dcec5a3e6d6e472273f3e80191111a3e
Flaymed <mvigg8@gmail.com>
2020-03-13 23:11:15 -0400

    MySQL Connection

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
 const kingdomData = require("./apiRoutes/kingdomData.js");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "website",
+  password: "IU*(&THHGkjhgfkjg&",
+  database: "kingsroyale"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Successfully connected to the database!");
+
+  //CREATE USERACCOUNT TABLE
+  con.query("CREATE TABLE IF NOT EXISTS userAccounts (username VARCHAR(255), password VARCHAR(255))");
+  //CREATE KINGDOM TABLE
+  con.query("CREATE TABLE IF NOT EXISTS kingdoms (owner VARCHAR(255), name VARCHAR(255), provinces INT(2), towers INT(3), mines INT(3), gold BIGINT, population BIGINT)");
+});
+
 
 app.enable('verbose errors');
 require('events').EventEmitter.defaultMaxListeners = 0;
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());   
+app.use(express.json());
 
 app.use('/api/kingdom/', kingdomData);
 
