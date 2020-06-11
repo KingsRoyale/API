a88fd121abbbcfd2c73d96f1d660b86223713329
Flaymed <mvigg8@gmail.com>
2020-06-11 15:31:09 -0400

    Verification Fixed

--- apiRoutes/kingdomData.js
+++ apiRoutes/kingdomData.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const api = express.Router();
+const kingdomManager = require("D:/Coding/KingsRoyale/API/utils/kingdomManager.js");
 const db = require("D:/Coding/KingsRoyale/API/utils/database.js");
 const con = db.getConnection();
+
 
 api.get('/:kname?', function (req, res) {
   var kname = req.params.kname;
@@ -22,37 +24,43 @@ api.post('/createKingdom', function (req, res) {
   var pswd = req.body.user.pswd;
   var kname = req.body.kingdom.name;
 
-  con.query("SELECT password FROM userAccounts WHERE username=?", [owner], function (error, result) {
+  //Verify the person has an account/their who they say they are.
+  con.query("SELECT * FROM userAccounts WHERE username=?", [owner], function (error, result) {
     if (error) throw error;
-
-    if (result[0] == null || result[0] == undefined) {
+    let account = result[0];
+    if (account == null || account == undefined) {
       res.writeHead( 800, 'Account does not exist', {'content-type' : 'text/plain'});
       res.end( 'Account does not exist!');
+    } else {
+      if (account.password != pswd) {
+        res.writeHead( 803, 'Username or password is incorrect!', {'content-type' : 'text/plain'});
+        res.end( 'Username or password is incorrect!');
+      }
     }
-
   });
 
+  //Check if the person already has a kingdom
   con.query("SELECT * FROM kingdoms WHERE owner=?", [username], function (err, result) {
     if (err) throw err;
-    if (result[0].name == null || result == undefined) {
-      res.send("already own kingdom");
-      return;
+    let kingdom = result[0];
+    if (kingdom != null || kingdom != undefined) {
+      res.writeHead( 901, 'Kingdom owner already has a kingdom', {'content-type' : 'text/plain'});
+      res.end( 'Kingdom owner already has a kingdom!');
     }
   });
 
+  //Check if kingdom name is taken
   con.query("SELECT * FROM kingdoms WHERE name=?", [kname], function (err, result) {
-    if (result == null || result == undefined) {
-      res.send("kingdom exists");
-      return;
+    if (err) throw err;
+    let kingdom = result[0];
+    if (kingdom != null || kingdom != undefined) {
+      res.writeHead( 902, 'Kingdom name is taken!', {'content-type' : 'text/plain'});
+      res.end( 'Kingdom name is taken!');
     }
   });
 
-  //gets here if the kingdom can be created!
-  con.query("INSERT INTO kingdoms (owner, name, provinces, towers, mines, gold, population) VALUES (?, ?, ?, ?, ?, ?, ?)", [owner, kname, 0, 0, 0, 1000, 100], function(error, response) {
-    if (error) throw error;
-    console.log(`[Kingdom] Kingdom was created with the name: ${kname}`);
-  });
-
+  //Calls if the user is able to create a kingdom.
+  kingdomManager.createKingdom(owner, kname);
 });
 
 module.exports = api
