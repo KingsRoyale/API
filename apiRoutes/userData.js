7a63d6046c2717e3fda57be0a7fb985e12426193
Flaymed <mvigg8@gmail.com>
2020-06-27 16:11:39 -0400

    Response Manager in userData

--- apiRoutes/userData.js
+++ apiRoutes/userData.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express.Router();
+const responseManager = require("D:/Coding/KingsRoyale/API/utils/managers/responseManager.js");
 const db = require("D:/Coding/KingsRoyale/API/utils/database.js");
 const con = db.getConnection();
 
@@ -10,7 +11,7 @@ app.post("/createAccount", function (req, res) {
   con.query("SELECT * FROM userAccounts WHERE username=?", [uname], function(error, account) {
     if (error) throw error;
     if (account != null || accoutnt != undefined) {
-      res.status
+      responseManager.usernameTaken(res);
     }
   })
 
